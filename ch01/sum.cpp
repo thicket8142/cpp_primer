30cff86b8f0ec237c882f3d3f1519f7c947046fc
rileydog <coderjob@robomail.xyz>
2025-01-12 10:37:15 -0500

    Correct increment error

--- ch01/sum.cpp
+++ ch01/sum.cpp
@@ -4,8 +4,8 @@ int main()
 {
 	int sum = 0, val = 50;
 
-	while (val++ <= 100) {
-		sum += val;
+	while (val <= 100) {
+		sum += val++;
 	}
 
 	std::cout << "The sum of the numbers from 50 to 100 is " << sum 
